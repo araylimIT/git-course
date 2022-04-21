d9c5f641da45a926ff32b1da0e2419290e7eb0af
araylimIT <103751239+araylimIT@users.noreply.github.com>
2022-04-21 15:04:52 +0300

    Update Account.h

--- Account.h
+++ Account.h
@@ -1,46 +1,12 @@
 #pragma once
 class Account {
 private:
-	double balance, interest, intRate;
-	int transactions;
+	
 public:
-	//Constructor
-	Account(double rate = 0.045, double bal = 0.0) {
-		balance = bal;
-		intRate = rate;
-		interest = 0.0;
-		transactions = 0;
-	}
-	//make a method for deposit
-	void makeDeposit(double amount) {
-		balance += amount;
-		transactions++;
-	}
 
-	//make a method for withdrawal
-
-	bool withdraw(double amount); // we provide 
-
-	void calculateInterest() {
-		interest = balance + intRate;
-		balance += interest;
-
-	}
-
-	//now make a method to get balance in main
+	double balance = 0.00;
 
 	double getBalance() {
 		return balance;
-	}
-
-	// make a method to get interest in main
-	double getInterest() {
-		return interest;
-	}
-
-	//make a method to get transactions in main
-
-	int getTransactions() {
-		return transactions;
 	}
 };
