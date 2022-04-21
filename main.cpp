637171e9b3584c1c1b3490d2a992d28e06698f25
araylimIT <103751239+araylimIT@users.noreply.github.com>
2022-04-21 15:05:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <iomanip>
 #include "Account.h"
-
+#include "AccountBalance.h"
+#include "AccountWithdraw.h"
 using namespace std;
 
 // make a display menu function
@@ -9,13 +10,13 @@ using namespace std;
 void displayMenu() {
 	cout << "\n\n            MENU\n\n";
 	cout << "1) Display the account balance\n";
-	cout << "2) Display the number of transactions\n";
-	cout << "3) Display interest earned for this period\n";
-	cout << "4) Nake a deposit\n";
-	cout << "5) Make a withdraw\n";
-	cout << "6) Add interest for this period\n";
-	cout << "7) Exit \n\n";
-	cout << "Enter your choice";
+	//cout << "2) Display the number of transactions\n";
+	//cout << "3) Display interest earned for this period\n";
+	cout << "2) Nake a deposit\n";
+	cout << "3) Make a withdraw\n";
+	//cout << "6) Add interest for this period\n";
+	cout << "4) Exit \n\n";
+	cout << "Enter your choice ";
 }
 
 // make a method to get choice validate choice and return choice to main method
@@ -35,7 +36,7 @@ char getChoice(char max) {
 
 // now make a methof for deposit which has an object parameter
 
-void makeDeposit(Account& account) {
+void makeDeposit(AccountBalance& account) {
 	double dollars;
 	cout << "Enter the amount of deposit: ";
 	cin >> dollars;
@@ -46,7 +47,7 @@ void makeDeposit(Account& account) {
 
 // make a method for withdrawal
 
-void withdraw(Account &account) {
+void withdraw(AccountWithdraw &account) {
 	double dollars;
 	cout << "Enter the amount of withdrawl: ";
 	cin >> dollars;
@@ -61,9 +62,11 @@ void withdraw(Account &account) {
 // after completing these methods we make a main method of this application
 
 int main() {
-	const char MAX_CHOICE = '7';
+	const char MAX_CHOICE = '4';
 	Account savings;
 	char choice;
+	AccountBalance save;
+	AccountWithdraw savi;
 	cout << fixed << showpoint << setprecision(2);
 	do {
 		displayMenu();
@@ -74,24 +77,15 @@ int main() {
 			cout << "The current balance is $";
 			cout << savings.getBalance() << endl;
 			break;
+		
 		case'2':
-			cout << "There have been ";
-			cout << savings.getTransactions() << "transactions. \n";
+			makeDeposit(save);
 			break;
 		case'3':
-			cout << "Interest earned for this period: $";
-			cout << savings.getInterest() << endl;
+			withdraw(savi);
 			break;
-		case'4':
-			makeDeposit(savings);
-			break;
-		case'5':
-			withdraw(savings);
-			break;
-		case'6':
-			savings.calculateInterest();
-			cout << "Interest added.\n";
+		
 		}
-	} while (choice != '7');
+	} while (choice != '4');
 	return 0;
 }
