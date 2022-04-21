377d290c8b2a5d31415d8235fb317885f6b02cf3
araylimIT <103751239+araylimIT@users.noreply.github.com>
2022-04-21 15:04:05 +0300

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -1,12 +1,1 @@
-#include "Account.h"
 
-bool Account::withdraw(double amount) {
-	if (balance < amount) {
-		return false;
-	}
-	else {
-		balance -= amount;
-		transactions++;
-		return true;
-	}
-}
