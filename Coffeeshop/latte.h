29d7d6bf2e46da0a3bc87e74aa1f99a763718e1d
araylimIT <103751239+araylimIT@users.noreply.github.com>
2022-04-24 15:32:36 +0300

    Update latte.h

--- Coffeeshop/latte.h
+++ Coffeeshop/latte.h
@@ -3,12 +3,11 @@
 
 class Latte : public IDrink
 {
-public:
-    Latte(double price = 1.8);
-    virtual double price();
-
 protected:
     virtual void recStepTwo();
     virtual void recStepFour();
+public:
+    Latte(double price = 1.8);
+    virtual double price();
 };
 
